7347caba0dd91545671abd21228a6cf069a527d1
Lisa Pscolkoski <lpscol01@gmail.com>
2014-03-05 15:23:21 -0500

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228013945) do
+ActiveRecord::Schema.define(version: 20140305201451) do
 
   create_table "posts", force: true do |t|
     t.integer  "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140228013945) do
   create_table "users", force: true do |t|
     t.string   "email"
     t.datetime "birthday"
+    t.string   "password"
   end
 
 end
